1a9c2d1bc88dbb998fe416a8a1306fce46bf351e
Kristina Kuzmenko <kristina@aidence.com>
2022-05-30 16:02:36 +0200

    Add mock service worker setup

---
+++ src/mocks/handlers.js
@@ -0,0 +1,18 @@
+import { rest } from "msw";
+
+export const handlers = [
+  rest.get("http://localhost:3030/scoops", (request, response, context) => {
+    return response(
+      context.json([
+          {
+          "name": "Vanilla",
+          "imagePath": "/images/vanilla.png"
+          },
+          {
+            "name": "Chocolate",
+            "imagePath": "/images/chocolate.png"
+          },
+        ])
+    );
+  }),
+];
